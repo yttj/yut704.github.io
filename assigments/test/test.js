04dbd42ac2e7b32942c9a31aa8e4edc210db7be8
yttj <yut704@newschool.edu>
2022-05-13 12:39:31 -0400

    update

--- assigments/test/test.js
+++ assigments/test/test.js
@@ -1,6 +1,18 @@
+document.getElementById("colored-element").style.color = "#32a852";
+
+const childElement = document.createElement('p')
+    childElement.innerHTML = "Hope I get good grades"
+    document.getElementById("parent-element").appendChild(childElement);
+
+
+
 const forLoopArray = ["Item One", "Item Two", "Item Three", "Item Four"]
 for (let i = 0; i < forLoopArray.length; i++) {
     console.log(forLoopArray[i])
   }
-
   document.getElementById("colored-element").style.color = "#DDA0DD";
+
+  const clickEventElement = document.querySelector('#event-listener-button')
+  clickEventElement.addEventListener('click', (event) => {
+      console.log('click event', event)
+  })
